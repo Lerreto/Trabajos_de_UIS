61992daa672375c519673ffa57001dc7a5d34f1d
Lerreto <juanpabloruedaangarita@gmail.com>
2025-03-20 10:49:44 -0500

    Punto 1 del taller

--- Taller_1/Punto_1.py
+++ Taller_1/Punto_1.py
@@ -6,21 +6,20 @@ pares dividida por el número de estos.
 
 """
 
-mensaje = True
 dato = ""
 suma = 0
 contandor = 0
 
-while mensaje:
+while True:
     
-    dato = int(input("Ingrese el numero entero par (-1 para salir) = "))
+    dato = int(input("Ingrese el numero entero par (0 para salir) = "))
+    
+    if dato == 0:
+        break
 
     if type(dato) == int and dato % 2 == 0:
         suma += dato
         contandor += 1
-
-    elif dato == -1:
-        mensaje = False
 
     else:
         print("\nADVERTENCIA = Debes de ingresar un numero par\n")
