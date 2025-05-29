d53467cb50af69685d334049235291d1cbb7f6c4
Joshep Jared Lizarazo Monterto <joshep2250150@correo.uis.edu.co>
2025-05-28 21:13:32 -0500

    Update avance del proyecto

--- Proyecto_Sudoku/Pruebas/hola2.py
+++ Proyecto_Sudoku/Pruebas/hola2.py
@@ -17,9 +17,44 @@ sudoku = np.array([
 
 tam = np.shape(sudoku)[0]
 
-
-
-
+def númerosolo(A, B, C):
+    doublessimp = []
+    convun = []
+    temptam = np.shape(A)[0]
+    for p in range():
+        listatemp = []
+        listatemp = np.setdiff1d(A[p], C[0])
+        listatemp = np.setdiff1d(listatemp[0], sudoku[(B[p, 0])])
+        listatemp = np.setdiff1d(listatemp[0], sudoku[:, (B[p], 1)])
+        doublessimp.append(listatemp)
+    
+    doublessimp2 = np.array(doublessimp)
+    
+    par = np.array(list(range(1,10)))
+    
+    for q in range(temptam):
+        par = np.intersect1d(par[0], doublessimp2[q])
+    
+    print(f"El par para el bloque es: {par}")
+    
+    
+    parapp = np.array(list(par))
+     
+    for F in range(temptam):
+        if F < temptam-2:
+            conum = np.setdiff1d(doublessimp2[F], parapp[0])
+            np.append(parapp, conum[0])
+            convun.append(conum[0])
+        else:
+            convun.append(conum[0])
+    
+    return convun
+            
+        
+        
+        
+        
+        
 
 while True:
 
@@ -27,15 +62,19 @@ while True:
         for j in range(0, tam, 3):
             doubles = []
             posiciones = []
+            descartados = []
             for g in range(i, i+3):
                 for m in range(j, i+3):
                     if sudoku[g, m] == 0:
                         pos = [g, m]
                         lista = list(range(1, 10))
                         doubles.append(lista)
-                        posiciones.append(pos)               
+                        posiciones.append(pos)
+                    else:
+                        descartados.append(sudoku[g, m])
+                               
+            númerosolo(np.array(doubles), np.array(posiciones), np.array(descartados))                    
             print(np.array(doubles))
             print(np.array(posiciones))
-
 
 
